e14a26355a730c40d3c8de90f3c8f3d3378232a7
AntoninaHoncharenko <g.antonina.vl@gmail.com>
2022-11-10 19:52:29 +0200

    create forecast markup

--- src/scripts/createMarkupForecast.js
+++ src/scripts/createMarkupForecast.js
@@ -1,3 +1,30 @@
 import { format } from 'date-fns';
 
-export function createMarkupForecast() {}
+export function createMarkupForecast() {
+  return /*html*/ `<li class="forecast-item">
+        <p class="forecast-date"></p>
+        <div class="temp-wrap">
+          <p class="forecast-main-temp"></p>
+          <img src="#" alt="1" class="forecast-img" />
+        </div>
+        <p class="forecast-feel-temp"></p>
+        <div class="forecast-info-container">
+          <div class="forecast-parameters">
+            <p class="forecast-info">Humidity</p>
+            <p class="forecast-info">Wind</p>
+            <p class="forecast-info">Sunrise</p>
+            <p class="forecast-info">Sunset</p>
+          </div>
+          <div class="forecast-values">
+            <p class="forecast-info">${main.humidity} %</p>
+            <p class="forecast-info">${wind.speed.toFixed(0)} m/s</p>
+            <p class="forecast-info">
+              ${format(new Date(sys.sunrise * 1000), 'HH:mm')}
+            </p>
+            <p class="forecast-info">
+              ${format(new Date(sys.sunset * 1000), 'HH:mm')}
+            </p>
+          </div>
+        </div>
+      </li>`;
+}
